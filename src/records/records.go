06c39bc0d444b46a0d450b21c854a4a131cb5785
Viking2012 <Viking2012@gmail.com>
2021-08-02 14:51:45 -0400

    add PurchaseRecords structs with sorting capability

---
+++ src/records/records.go
@@ -0,0 +1,125 @@
+package records
+
+import (
+	"sort"
+	"time"
+)
+
+const layout = "2006-01-02"
+
+// A PurchaseRecord contains all of the information on individual purchases and their prices to identify outliers
+type PurchaseRecord struct {
+	Uuid               int64
+	ProductID          string
+	CustomerID         string
+	PurchaseDate       time.Time
+	DocumentNumber     int64
+	DocumentLineNumber int64
+	Price              float64
+}
+
+// implementation basics from https://pkg.go.dev/sort#example-package-SortKeys
+// lessFunc is the type of a "less" function that defines the ordering of its PurchaseRecord arguments.
+type lessFunc func(p1, p2 *PurchaseRecord) bool
+
+// multiSorter implements the Sort interface, sorting the changes within.
+type multiSorter struct {
+	records []PurchaseRecord
+	less    []lessFunc
+}
+
+// Sort sorts the argument slice according to the less functions passed to OrderedBy.
+func (ms *multiSorter) Sort(records []PurchaseRecord) {
+	ms.records = records
+	sort.Sort(ms)
+}
+
+// OrderedBy returns a Sorter that sorts using the less functions, in order.
+// Call its Sort method to sort the data.
+func OrderedBy(less ...lessFunc) *multiSorter {
+	return &multiSorter{
+		less: less,
+	}
+}
+
+// Len is part of sort.Interface.
+func (ms *multiSorter) Len() int {
+	return len(ms.records)
+}
+
+// Swap is part of sort.Interface
+func (ms *multiSorter) Swap(i, j int) {
+	ms.records[i], ms.records[j] = ms.records[j], ms.records[i]
+}
+
+// Less is part of sort.Interface. It is implemented by looping along the
+// less functions until it finds a comparison that discriminates between
+// the two items (one is less than the other). Note that it can call the
+// less functions twice per call. We could change the functions to return
+// -1, 0, 1 and reduce the number of calls for greater efficiency: an
+// exercise for the reader.
+func (ms *multiSorter) Less(i, j int) bool {
+	p, q := &ms.records[i], &ms.records[j]
+	// Try all but the last comparison.
+	var k int
+	for k = 0; k < len(ms.less)-1; k++ {
+		less := ms.less[k]
+		switch {
+		case less(p, q):
+			// p < q, so we have a decision.
+			return true
+		case less(q, p):
+			// p > q, so we have a decision.
+			return false
+		}
+		// p == q; try the next comparison.
+	}
+	// All comparisons to here said "equal", so just return whatever
+	// the final comparison reports.
+	return ms.less[k](p, q)
+}
+
+func byUuid(p1, p2 *PurchaseRecord) bool {
+	return p1.Uuid < p2.Uuid
+}
+
+func byProduct(p1, p2 *PurchaseRecord) bool {
+	return p1.ProductID < p2.ProductID
+}
+
+func byCustomer(p1, p2 *PurchaseRecord) bool {
+	return p1.CustomerID < p2.CustomerID
+}
+
+func byDate(p1, p2 *PurchaseRecord) bool {
+	return p1.PurchaseDate.Before(p2.PurchaseDate)
+}
+
+func byDocumentNumber(p1, p2 *PurchaseRecord) bool {
+	return p1.DocumentNumber < p2.DocumentNumber
+}
+
+func byDocumentLineNumber(p1, p2 *PurchaseRecord) bool {
+	return p1.DocumentLineNumber < p2.DocumentLineNumber
+}
+
+func byPrice(p1, p2 *PurchaseRecord) bool {
+	return p1.Price < p2.Price
+}
+
+var ByUuid lessFunc = byUuid
+var ByProduct lessFunc = byProduct
+var ByCustomer lessFunc = byCustomer
+var ByDate lessFunc = byDate
+var ByDocumentNumber lessFunc = byDocumentNumber
+var ByDocumentLineNumber lessFunc = byDocumentLineNumber
+var ByPrice lessFunc = byPrice
+
+// QuickParse simply reutrns a result of parsing a time string in YYYY-MM-DD format
+func QuickParse(dateString string) time.Time {
+	t, err := time.Parse(layout, dateString)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
---
+++ src/records/records_test.go
@@ -0,0 +1,325 @@
+package records
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+var RawRecords = []PurchaseRecord{
+	{Uuid: 0, ProductID: "bed_bath_table:8", CustomerID: "15df0", PurchaseDate: QuickParse("2017-02-28"), DocumentNumber: 100000000, DocumentLineNumber: 1, Price: 101.14},
+	{Uuid: 1, ProductID: "bed_bath_table:8", CustomerID: "f4c13", PurchaseDate: QuickParse("2017-02-28"), DocumentNumber: 100000100, DocumentLineNumber: 1, Price: 104.70},
+	{Uuid: 2, ProductID: "bed_bath_table:9", CustomerID: "0dc4b", PurchaseDate: QuickParse("2017-03-01"), DocumentNumber: 100000200, DocumentLineNumber: 1, Price: 101.14},
+	{Uuid: 3, ProductID: "bed_bath_table:8", CustomerID: "d98e2", PurchaseDate: QuickParse("2017-03-02"), DocumentNumber: 100000300, DocumentLineNumber: 1, Price: 104.70},
+	{Uuid: 4, ProductID: "bed_bath_table:8", CustomerID: "2ed85", PurchaseDate: QuickParse("2017-03-04"), DocumentNumber: 100000400, DocumentLineNumber: 1, Price: 101.14},
+	{Uuid: 5, ProductID: "bed_bath_table:9", CustomerID: "6058d", PurchaseDate: QuickParse("2017-03-05"), DocumentNumber: 100000500, DocumentLineNumber: 1, Price: 106.23},
+	{Uuid: 6, ProductID: "bed_bath_table:8", CustomerID: "f4c13", PurchaseDate: QuickParse("2017-03-06"), DocumentNumber: 100000600, DocumentLineNumber: 1, Price: 101.14},
+	{Uuid: 7, ProductID: "bed_bath_table:8", CustomerID: "d5f2b", PurchaseDate: QuickParse("2017-03-06"), DocumentNumber: 100000700, DocumentLineNumber: 1, Price: 101.14},
+	{Uuid: 8, ProductID: "bed_bath_table:8", CustomerID: "0d554", PurchaseDate: QuickParse("2017-03-08"), DocumentNumber: 100000800, DocumentLineNumber: 1, Price: 101.14},
+	{Uuid: 9, ProductID: "bed_bath_table:8", CustomerID: "6d52f", PurchaseDate: QuickParse("2017-03-09"), DocumentNumber: 100000900, DocumentLineNumber: 1, Price: 115.02},
+	{Uuid: 10, ProductID: "bed_bath_table:9", CustomerID: "679f8", PurchaseDate: QuickParse("2017-03-11"), DocumentNumber: 100001000, DocumentLineNumber: 1, Price: 106.23},
+	{Uuid: 11, ProductID: "bed_bath_table:9", CustomerID: "5af63", PurchaseDate: QuickParse("2017-03-13"), DocumentNumber: 100001100, DocumentLineNumber: 1, Price: 101.14},
+	{Uuid: 12, ProductID: "bed_bath_table:9", CustomerID: "61e64", PurchaseDate: QuickParse("2017-03-13"), DocumentNumber: 100001200, DocumentLineNumber: 1, Price: 104.70},
+	{Uuid: 13, ProductID: "bed_bath_table:9", CustomerID: "5af63", PurchaseDate: QuickParse("2017-03-16"), DocumentNumber: 100001300, DocumentLineNumber: 1, Price: 102.18},
+	{Uuid: 14, ProductID: "bed_bath_table:9", CustomerID: "68fe3", PurchaseDate: QuickParse("2017-03-16"), DocumentNumber: 100001400, DocumentLineNumber: 1, Price: 101.14},
+	{Uuid: 15, ProductID: "bed_bath_table:8", CustomerID: "f4c13", PurchaseDate: QuickParse("2017-03-20"), DocumentNumber: 100001500, DocumentLineNumber: 1, Price: 104.70},
+	{Uuid: 16, ProductID: "bed_bath_table:9", CustomerID: "d98e2", PurchaseDate: QuickParse("2017-03-20"), DocumentNumber: 100001600, DocumentLineNumber: 1, Price: 101.14},
+	{Uuid: 17, ProductID: "bed_bath_table:9", CustomerID: "d98e2", PurchaseDate: QuickParse("2017-03-20"), DocumentNumber: 100001600, DocumentLineNumber: 2, Price: 101.18},
+	{Uuid: 18, ProductID: "bed_bath_table:8", CustomerID: "4ab4d", PurchaseDate: QuickParse("2017-03-23"), DocumentNumber: 100001700, DocumentLineNumber: 1, Price: 101.14},
+	{Uuid: 19, ProductID: "bed_bath_table:9", CustomerID: "20dcb", PurchaseDate: QuickParse("2017-03-27"), DocumentNumber: 100001800, DocumentLineNumber: 1, Price: 106.23},
+}
+
+func verifyIntegerOrder(want, got []int64) (bool, string, error) {
+	checkLen := len(want)
+	var allSame bool = true
+
+	if checkLen != len(got) {
+		return false, "", errors.New("Different lengths of original records and sorted records")
+	}
+
+	outputString := fmt.Sprintf("\n%9s %-9s\n", "wanted:", "got:")
+	for i := 0; i < checkLen; i++ {
+		w := want[i]
+		g := got[i]
+
+		if w != g {
+			allSame = false
+			wString := fmt.Sprintf("***%6d", w)
+			gString := fmt.Sprintf("%-6d***", g)
+			outputString = fmt.Sprintf("%s%s %s\n", outputString, wString, gString)
+		} else {
+			outputString = fmt.Sprintf("%s%9d %-9d\n", outputString, w, g)
+		}
+	}
+
+	return allSame, outputString, nil
+}
+
+func verifyFloatOrder(want, got []float64) (bool, string, error) {
+	checkLen := len(want)
+	var allSame bool = true
+
+	if checkLen != len(got) {
+		return false, "", errors.New("Different lengths of original records and sorted records")
+	}
+
+	outputString := fmt.Sprintf("\n%9s %-9s\n", "wanted:", "got:")
+	for i := 0; i < checkLen; i++ {
+		w := want[i]
+		g := got[i]
+
+		if w != g {
+			allSame = false
+			wString := fmt.Sprintf("***%6.2f", w)
+			gString := fmt.Sprintf("%-6.2f***", g)
+			outputString = fmt.Sprintf("%s%s %s\n", outputString, wString, gString)
+		} else {
+			outputString = fmt.Sprintf("%s%9.2f %-9.2f\n", outputString, w, g)
+		}
+	}
+
+	return allSame, outputString, nil
+}
+
+func verifyStringOrder(want, got []string) (bool, string, error) {
+	checkLen := len(want)
+	var allSame bool = true
+
+	if checkLen != len(got) {
+		return false, "", errors.New("Different lengths of original records and sorted records")
+	}
+
+	outputString := fmt.Sprintf("\n%19s %-19s\n", "wanted:", "got:")
+	for i := 0; i < checkLen; i++ {
+		w := want[i]
+		g := got[i]
+
+		if w != g {
+			allSame = false
+			wString := fmt.Sprintf("***%16s", w)
+			gString := fmt.Sprintf("%-16s***", g)
+			outputString = fmt.Sprintf("%s%s %s\n", outputString, wString, gString)
+		} else {
+			outputString = fmt.Sprintf("%s%19s %-19s\n", outputString, w, g)
+		}
+	}
+
+	return allSame, outputString, nil
+}
+
+func TestOrderByUuid(t *testing.T) {
+	want := []int64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
+	s := make([]PurchaseRecord, len(RawRecords))
+	copy(s, RawRecords)
+
+	OrderedBy(ByUuid).Sort(s)
+
+	r := make([]int64, len(s))
+	for i := range r {
+		r[i] = s[i].Uuid
+	}
+
+	same, outputString, err := verifyIntegerOrder(want, r)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !same {
+		t.Errorf(outputString)
+	}
+}
+
+func TestOrderByProductID(t *testing.T) {
+	want := []string{"bed_bath_table:8", "bed_bath_table:8", "bed_bath_table:8", "bed_bath_table:8", "bed_bath_table:8", "bed_bath_table:8", "bed_bath_table:8", "bed_bath_table:8", "bed_bath_table:8", "bed_bath_table:8", "bed_bath_table:9", "bed_bath_table:9", "bed_bath_table:9", "bed_bath_table:9", "bed_bath_table:9", "bed_bath_table:9", "bed_bath_table:9", "bed_bath_table:9", "bed_bath_table:9", "bed_bath_table:9"}
+	s := make([]PurchaseRecord, len(RawRecords))
+	copy(s, RawRecords)
+
+	OrderedBy(ByProduct).Sort(s)
+
+	r := make([]string, len(s))
+	for i := range r {
+		r[i] = s[i].ProductID
+	}
+
+	same, outputString, err := verifyStringOrder(want, r)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !same {
+		t.Errorf(outputString)
+	}
+}
+
+func TestOrderByCustomerID(t *testing.T) {
+	want := []string{"0d554", "0dc4b", "15df0", "20dcb", "2ed85", "4ab4d", "5af63", "5af63", "6058d", "61e64", "679f8", "68fe3", "6d52f", "d5f2b", "d98e2", "d98e2", "d98e2", "f4c13", "f4c13", "f4c13"}
+	s := make([]PurchaseRecord, len(RawRecords))
+	copy(s, RawRecords)
+
+	OrderedBy(ByCustomer).Sort(s)
+
+	r := make([]string, len(s))
+	for i := range r {
+		r[i] = s[i].CustomerID
+	}
+
+	same, outputString, err := verifyStringOrder(want, r)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !same {
+		t.Errorf(outputString)
+	}
+}
+
+func TestOrderByPurchaseDate(t *testing.T) {
+	want := []string{"2017-02-28", "2017-02-28", "2017-03-01", "2017-03-02", "2017-03-04", "2017-03-05", "2017-03-06", "2017-03-06", "2017-03-08", "2017-03-09", "2017-03-11", "2017-03-13", "2017-03-13", "2017-03-16", "2017-03-16", "2017-03-20", "2017-03-20", "2017-03-20", "2017-03-23", "2017-03-27"}
+	s := make([]PurchaseRecord, len(RawRecords))
+	copy(s, RawRecords)
+
+	OrderedBy(ByDate).Sort(s)
+
+	r := make([]string, len(s))
+	for i := range r {
+		r[i] = s[i].PurchaseDate.Format(layout)
+	}
+
+	same, outputString, err := verifyStringOrder(want, r)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !same {
+		t.Errorf(outputString)
+	}
+}
+
+func TestOrderByDocumentNumber(t *testing.T) {
+	want := []int64{100000000, 100000100, 100000200, 100000300, 100000400, 100000500, 100000600, 100000700, 100000800, 100000900, 100001000, 100001100, 100001200, 100001300, 100001400, 100001500, 100001600, 100001600, 100001700, 100001800}
+	s := make([]PurchaseRecord, len(RawRecords))
+	copy(s, RawRecords)
+
+	OrderedBy(ByDocumentNumber).Sort(s)
+
+	r := make([]int64, len(s))
+	for i := range r {
+		r[i] = s[i].DocumentNumber
+	}
+
+	same, outputString, err := verifyIntegerOrder(want, r)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !same {
+		t.Errorf(outputString)
+	}
+}
+
+func TestOrderByDocumentLineNumber(t *testing.T) {
+	want := []int64{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2}
+	s := make([]PurchaseRecord, len(RawRecords))
+	copy(s, RawRecords)
+
+	OrderedBy(ByDocumentLineNumber).Sort(s)
+
+	r := make([]int64, len(s))
+	for i := range r {
+		r[i] = s[i].DocumentLineNumber
+	}
+
+	same, outputString, err := verifyIntegerOrder(want, r)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !same {
+		t.Errorf(outputString)
+	}
+}
+
+func TestOrderByPrice(t *testing.T) {
+	want := []float64{101.14, 101.14, 101.14, 101.14, 101.14, 101.14, 101.14, 101.14, 101.14, 101.14, 101.18, 102.18, 104.7, 104.7, 104.7, 104.7, 106.23, 106.23, 106.23, 115.02}
+	s := make([]PurchaseRecord, len(RawRecords))
+	copy(s, RawRecords)
+
+	OrderedBy(ByPrice).Sort(s)
+
+	r := make([]float64, len(s))
+	for i := range r {
+		r[i] = s[i].Price
+	}
+
+	same, outputString, err := verifyFloatOrder(want, r)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !same {
+		t.Errorf(outputString)
+	}
+}
+
+func TestOrderByProductCustomerDocumentDatePrice(t *testing.T) {
+	want := []int64{8, 0, 4, 18, 9, 7, 3, 1, 6, 15, 2, 19, 11, 13, 5, 12, 10, 14, 16, 17}
+	s := make([]PurchaseRecord, len(RawRecords))
+	copy(s, RawRecords)
+
+	OrderedBy(ByProduct, ByCustomer, ByDate, ByDocumentNumber, ByDocumentLineNumber, ByPrice).Sort(s)
+
+	r := make([]int64, len(s))
+	for i := range r {
+		r[i] = s[i].Uuid
+	}
+
+	same, outputString, err := verifyIntegerOrder(want, r)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !same {
+		t.Errorf(outputString)
+	}
+}
+
+func TestOrderByDocumentAndLineNumber(t *testing.T) {
+	want := []int64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
+	s := make([]PurchaseRecord, len(RawRecords))
+	copy(s, RawRecords)
+
+	OrderedBy(ByDocumentNumber, ByDocumentLineNumber).Sort(s)
+
+	r := make([]int64, len(s))
+	for i := range r {
+		r[i] = s[i].Uuid
+	}
+
+	same, outputString, err := verifyIntegerOrder(want, r)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !same {
+		t.Errorf(outputString)
+	}
+}
+
+func TestQuickPrasePanics(t *testing.T) {
+	// No need to check whether `recover()` is nil. Just turn off the panic.
+	defer func() { recover() }()
+
+	QuickParse("abc")
+
+	// Never reaches here if `OtherFunctionThatPanics` panics.
+	t.Errorf("did not panic")
+}
