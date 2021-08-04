c89b8550b44ee35fbdac78e572a918f28d2151f3
Viking2012 <Viking2012@gmail.com>
2021-08-04 11:00:29 -0400

    refactor field mapping extraction and create unit tests

--- src/readr/readr.go
+++ src/readr/readr.go
@@ -2,14 +2,48 @@ package readr
 
 import (
 	"encoding/csv"
+	"errors"
 	"os"
 	"strconv"
 
 	"github.com/Viking2012/goraynor/src/records"
 )
 
-// PraseCSV reads a csv at the given filepath and converts it to pricing records we can examine
-func ParseCSV(filepath string, headerRows int) ([]records.PriceRecord, error) {
+// FieldIndexMap provides a structure which enables readr to find data fields in the csv provided.
+// Seven fields are required to build records, but not all might be present in the data sent to this command.
+// Use a negative value for readr to "build" the field using the row index.
+type FieldIndexMap struct {
+	UUID               int
+	ProductID          int
+	CustomerID         int
+	PurchaseDate       int
+	DocumentNumber     int
+	DocumentLineNumber int
+	Price              int
+}
+
+var DefaultFieldMap FieldIndexMap = FieldIndexMap{
+	UUID:               0,
+	ProductID:          1,
+	CustomerID:         2,
+	PurchaseDate:       3,
+	DocumentNumber:     4,
+	DocumentLineNumber: 5,
+	Price:              6,
+}
+
+// ParseCSV reads a csv at the given filepath and converts it to pricing records we can examine
+func ParseCSV(filepath string, headerRows int, fieldMap *FieldIndexMap) ([]records.PriceRecord, error) {
+	// if fieldMap was a nil pointer, then use the default map
+	if fieldMap == nil {
+		fieldMap = &DefaultFieldMap
+	}
+
+	// Price MUST be provided - without it, what are we running this for?
+	if fieldMap.Price < 0 {
+		return []records.PriceRecord{}, errors.New("Index for price must be provided")
+	}
+
 	csvFile, err := os.Open(filepath)
 
 	if err != nil {
@@ -20,7 +54,9 @@ func ParseCSV(filepath string, headerRows int) ([]records.PriceRecord, error) {
 
 	lines, err := r.ReadAll()
 
-	clean, err := parseLines(lines[headerRows:])
+	var clean []records.PriceRecord
+
+	clean, err = parseLines(lines[headerRows:], fieldMap)
 
 	if err != nil {
 		return []records.PriceRecord{}, err
@@ -30,25 +66,58 @@ func ParseCSV(filepath string, headerRows int) ([]records.PriceRecord, error) {
 
 }
 
-// parseLines allows for easier testing (can take an array of an array of strings from any source location)
-func parseLines(lines [][]string) ([]records.PriceRecord, error) {
-	numLines := len(lines)
+func extractIntField(record []string, givenIndex, i int) (int, error) {
+	if givenIndex < 0 {
+		return i, nil
+	}
 
+	r, err := strconv.Atoi(record[givenIndex])
+	return r, err
+}
+
+func extractFloatField(record []string, givenIndex, i int) (float64, error) {
+	if givenIndex < 0 {
+		return float64(i), nil
+	}
+
+	r, err := strconv.ParseFloat(record[givenIndex], 64)
+	return r, err
+}
+
+// parseLines allows for easier testing (can take an array of an array of strings from any source location)
+func parseLines(lines [][]string, fieldMap *FieldIndexMap) ([]records.PriceRecord, error) {
+	numLines := len(lines)
 	priceRecords := make([]records.PriceRecord, numLines)
 
 	for i := 0; i < numLines; i++ {
-		thisPrice, err := strconv.ParseFloat(lines[i][3], 64)
+		thisUuid, err := extractIntField(lines[i], fieldMap.UUID, i)
 		if err != nil {
 			return []records.PriceRecord{}, err
 		}
+
+		thisPrice, err := extractFloatField(lines[i], fieldMap.Price, i)
+		if err != nil {
+			return []records.PriceRecord{}, err
+		}
+
+		thisDoc, err := extractIntField(lines[i], fieldMap.DocumentNumber, i)
+		if err != nil {
+			return []records.PriceRecord{}, err
+		}
+
+		thisDocNum, err := extractIntField(lines[i], fieldMap.DocumentLineNumber, i)
+		if err != nil {
+			return []records.PriceRecord{}, err
+		}
+
 		priceRecords[i] = records.PriceRecord{
-			Uuid:               int64(i),
-			ProductID:          lines[i][0],
-			CustomerID:         lines[i][1],
-			PurchaseDate:       records.QuickParse(lines[i][2]),
+			Uuid:               int64(thisUuid),
+			ProductID:          lines[i][fieldMap.ProductID],
+			CustomerID:         lines[i][fieldMap.CustomerID],
+			PurchaseDate:       records.QuickParse(lines[i][fieldMap.PurchaseDate]),
 			Price:              thisPrice,
-			DocumentNumber:     int64(i),
-			DocumentLineNumber: int64(1),
+			DocumentNumber:     int64(thisDoc),
+			DocumentLineNumber: int64(thisDocNum),
 		}
 	}
 
--- src/readr/readr_test.go
+++ src/readr/readr_test.go
@@ -8,20 +8,32 @@ import (
 	"github.com/Viking2012/goraynor/src/records"
 )
 
+// Reused testing variables
 var (
-	rawLines string = `productId,customerId,purchaseDate,totalPrice
+	rawCsvPath string = "../../test/test_data.csv"
+	rawLines   string = `productId,customerId,purchaseDate,totalPrice
 bed_bath_table:8,15df0,2017-02-28,101.14
 bed_bath_table:8,f4c13,2017-02-28,104.7
 bed_bath_table:9,0dc4b,2017-03-01,101.14
 bed_bath_table:8,d98e2,2017-03-02,104.7
 bed_bath_table:8,2ed85,2017-03-04,101.14
 `
+	rawFieldMap *FieldIndexMap = &FieldIndexMap{
+		UUID:               -1,
+		ProductID:          0,
+		CustomerID:         1,
+		PurchaseDate:       2,
+		DocumentNumber:     -1,
+		DocumentLineNumber: -1,
+		Price:              3,
+	}
+
 	priceRecords []records.PriceRecord = []records.PriceRecord{
-		{Uuid: 0, ProductID: "bed_bath_table:8", CustomerID: "15df0", PurchaseDate: records.QuickParse("2017-02-28"), DocumentNumber: 0, DocumentLineNumber: 1, Price: 101.14},
+		{Uuid: 0, ProductID: "bed_bath_table:8", CustomerID: "15df0", PurchaseDate: records.QuickParse("2017-02-28"), DocumentNumber: 0, DocumentLineNumber: 0, Price: 101.14},
 		{Uuid: 1, ProductID: "bed_bath_table:8", CustomerID: "f4c13", PurchaseDate: records.QuickParse("2017-02-28"), DocumentNumber: 1, DocumentLineNumber: 1, Price: 104.70},
-		{Uuid: 2, ProductID: "bed_bath_table:9", CustomerID: "0dc4b", PurchaseDate: records.QuickParse("2017-03-01"), DocumentNumber: 2, DocumentLineNumber: 1, Price: 101.14},
-		{Uuid: 3, ProductID: "bed_bath_table:8", CustomerID: "d98e2", PurchaseDate: records.QuickParse("2017-03-02"), DocumentNumber: 3, DocumentLineNumber: 1, Price: 104.70},
-		{Uuid: 4, ProductID: "bed_bath_table:8", CustomerID: "2ed85", PurchaseDate: records.QuickParse("2017-03-04"), DocumentNumber: 4, DocumentLineNumber: 1, Price: 101.14},
+		{Uuid: 2, ProductID: "bed_bath_table:9", CustomerID: "0dc4b", PurchaseDate: records.QuickParse("2017-03-01"), DocumentNumber: 2, DocumentLineNumber: 2, Price: 101.14},
+		{Uuid: 3, ProductID: "bed_bath_table:8", CustomerID: "d98e2", PurchaseDate: records.QuickParse("2017-03-02"), DocumentNumber: 3, DocumentLineNumber: 3, Price: 104.70},
+		{Uuid: 4, ProductID: "bed_bath_table:8", CustomerID: "2ed85", PurchaseDate: records.QuickParse("2017-03-04"), DocumentNumber: 4, DocumentLineNumber: 4, Price: 101.14},
 	}
 
 	badLine [][]string = [][]string{
@@ -29,10 +41,101 @@ bed_bath_table:8,2ed85,2017-03-04,101.14
 	}
 )
 
+// ParseCsv Testing
+
+func TestParseCsvSkipsLines(t *testing.T) {
+	parsed, err := ParseCSV(rawCsvPath, 1, &DefaultFieldMap)
+
+	if err != nil {
+		t.Errorf("ParseCsv should have skipped a header row which would have caused an error. Instead it returned: \n%s", err)
+	}
+
+	for i, got := range parsed {
+		want := priceRecords[i]
+		if want.Uuid != got.Uuid {
+			t.Errorf("For CSV Line %d, wanted               UUID: %d, but got: %d", i, want.Uuid, got.Uuid)
+		}
+		if want.ProductID != got.ProductID {
+			t.Errorf("For CSV Line %d, wanted          ProductID: %s, but got: %s", i, want.ProductID, got.ProductID)
+		}
+		if want.CustomerID != got.CustomerID {
+			t.Errorf("For CSV Line %d, wanted         CustomerID: %s, but got: %s", i, want.CustomerID, got.CustomerID)
+		}
+		if !want.PurchaseDate.Equal(got.PurchaseDate) {
+			t.Errorf("For CSV Line %d, wanted       PurchaseDate: %s, but got: %s", i, want.PurchaseDate.Format("2006-01-02"), got.PurchaseDate.Format("2006-01-02"))
+		}
+		if want.DocumentNumber != got.DocumentNumber {
+			t.Errorf("For CSV Line %d, wanted     DocumentNumber: %d, but got: %d", i, want.DocumentNumber, got.DocumentNumber)
+		}
+		if want.DocumentLineNumber != got.DocumentLineNumber {
+			t.Errorf("For CSV Line %d, wanted DocumentLineNumber: %d, but got: %d", i, want.DocumentLineNumber, got.DocumentLineNumber)
+		}
+		if want.Price != got.Price {
+			t.Errorf("For CSV Line %d, wanted              Price: %.2F, but got: %.2F", i, want.Price, got.Price)
+		}
+	}
+}
+
+func TestParseCsvUsesDefaultFieldMap(t *testing.T) {
+	parsed, err := ParseCSV(rawCsvPath, 1, nil)
+
+	if err != nil {
+		t.Errorf("ParseCsv should have skipped a header row which would have caused an error. Instead it returned: \n%s", err)
+	}
+
+	for i, got := range parsed {
+		want := priceRecords[i]
+		if want.Uuid != got.Uuid {
+			t.Errorf("For CSV Line %d, wanted               UUID: %d, but got: %d", i, want.Uuid, got.Uuid)
+		}
+		if want.ProductID != got.ProductID {
+			t.Errorf("For CSV Line %d, wanted          ProductID: %s, but got: %s", i, want.ProductID, got.ProductID)
+		}
+		if want.CustomerID != got.CustomerID {
+			t.Errorf("For CSV Line %d, wanted         CustomerID: %s, but got: %s", i, want.CustomerID, got.CustomerID)
+		}
+		if !want.PurchaseDate.Equal(got.PurchaseDate) {
+			t.Errorf("For CSV Line %d, wanted       PurchaseDate: %s, but got: %s", i, want.PurchaseDate.Format("2006-01-02"), got.PurchaseDate.Format("2006-01-02"))
+		}
+		if want.DocumentNumber != got.DocumentNumber {
+			t.Errorf("For CSV Line %d, wanted     DocumentNumber: %d, but got: %d", i, want.DocumentNumber, got.DocumentNumber)
+		}
+		if want.DocumentLineNumber != got.DocumentLineNumber {
+			t.Errorf("For CSV Line %d, wanted DocumentLineNumber: %d, but got: %d", i, want.DocumentLineNumber, got.DocumentLineNumber)
+		}
+		if want.Price != got.Price {
+			t.Errorf("For CSV Line %d, wanted              Price: %.2F, but got: %.2F", i, want.Price, got.Price)
+		}
+	}
+}
+
+func TestParseCsvCatchesFieldMapWithoutPriceIndex(t *testing.T) {
+	_, err := ParseCSV(rawCsvPath, 1, &FieldIndexMap{Price: -1})
+	if err == nil {
+		t.Errorf("ParseCsv should have errored on a FieldIndexMap without a price index, but didn't")
+	}
+}
+
+func TestParseCsvErrorsOnNonExistentFile(t *testing.T) {
+	_, err := ParseCSV("DoesNotExist", 1, nil)
+	if err == nil {
+		t.Errorf("ParseCsv should have errored when receiving a non-existent filepath, but didn't")
+	}
+}
+
+func TestParseCsvErrorsOnBadRecord(t *testing.T) {
+	// here, we use 0 for skipLines since we want to "incorrectly" convert the header into a PriceRecord
+	_, err := ParseCSV(rawCsvPath, 0, nil)
+	if err == nil {
+		t.Errorf("ParseCsv should have errored when trying to convert a bad record, but didn't")
+	}
+}
+
+// parseLines Testing
 func TestParseLines(t *testing.T) {
 	r := csv.NewReader(strings.NewReader(rawLines))
 	lines, err := r.ReadAll()
-	parsed, err := parseLines(lines[1:])
+	parsed, err := parseLines(lines[1:], rawFieldMap)
 
 	if err != nil {
 		t.Fatal(err)
@@ -65,42 +168,63 @@ func TestParseLines(t *testing.T) {
 }
 
 func TestParseLinesShouldRaiseFloatError(t *testing.T) {
-	_, err := parseLines(badLine)
+	_, err := parseLines(badLine, rawFieldMap)
 
 	if err == nil {
 		t.Error("parseLines should have raised a bad float conversion error, but didn't")
 	}
 }
 
-func TestParseCsvSkipsLines(t *testing.T) {
-	parsed, err := ParseCSV("../../test/test_data.csv", 1)
+func TestParseLinesErrorsOnBadUuid(t *testing.T) {
+	var badRecord [][]string = [][]string{{"BadUuid", "product_ok", "customer_ok", "2001-02-03", "1", "1", "100.00"}}
+	_, err := parseLines(badRecord, &DefaultFieldMap)
+	if err == nil {
+		t.Error("parseLines should have raised a bad UUID conversion error, but didn't")
+	}
+}
+func TestParseLinesErrorsOnBadPrice(t *testing.T) {
+	var badRecord [][]string = [][]string{{"1", "product_ok", "customer_ok", "2001-02-03", "1", "1", "BadPrice"}}
+	_, err := parseLines(badRecord, &DefaultFieldMap)
+	if err == nil {
+		t.Error("parseLines should have raised a bad UUID conversion error, but didn't")
+	}
+}
+func TestParseLinesErrorsOnBadDoc(t *testing.T) {
+	var badRecord [][]string = [][]string{{"1", "product_ok", "customer_ok", "2001-02-03", "BadDoc", "1", "100.00"}}
+	_, err := parseLines(badRecord, &DefaultFieldMap)
+	if err == nil {
+		t.Error("parseLines should have raised a bad UUID conversion error, but didn't")
+	}
+}
+func TestParseLinesErrorsOnBadDocLine(t *testing.T) {
+	var badRecord [][]string = [][]string{{"1", "product_ok", "customer_ok", "2001-02-03", "1", "BadLine", "100.00"}}
+	_, err := parseLines(badRecord, &DefaultFieldMap)
+	if err == nil {
+		t.Error("parseLines should have raised a bad UUID conversion error, but didn't")
+	}
+}
+
+// Extraction & Conversion Testing
+func TestExtractIntFieldHandlesNegativeIndex(t *testing.T) {
+	want := int(10)
+	got, err := extractIntField([]string{"99"}, -1, 10)
 
 	if err != nil {
 		t.Error(err)
 	}
+	if want != got {
+		t.Errorf("extractIntField should have returned %d, but got %d instead", want, got)
+	}
+}
 
-	for i, got := range parsed {
-		want := priceRecords[i]
-		if want.Uuid != got.Uuid {
-			t.Errorf("For CSV Line %d, wanted               UUID: %d, but got: %d", i, want.Uuid, got.Uuid)
-		}
-		if want.ProductID != got.ProductID {
-			t.Errorf("For CSV Line %d, wanted          ProductID: %s, but got: %s", i, want.ProductID, got.ProductID)
-		}
-		if want.CustomerID != got.CustomerID {
-			t.Errorf("For CSV Line %d, wanted         CustomerID: %s, but got: %s", i, want.CustomerID, got.CustomerID)
-		}
-		if !want.PurchaseDate.Equal(got.PurchaseDate) {
-			t.Errorf("For CSV Line %d, wanted       PurchaseDate: %s, but got: %s", i, want.PurchaseDate.Format("2006-01-02"), got.PurchaseDate.Format("2006-01-02"))
-		}
-		if want.DocumentNumber != got.DocumentNumber {
-			t.Errorf("For CSV Line %d, wanted     DocumentNumber: %d, but got: %d", i, want.DocumentNumber, got.DocumentNumber)
-		}
-		if want.DocumentLineNumber != got.DocumentLineNumber {
-			t.Errorf("For CSV Line %d, wanted DocumentLineNumber: %d, but got: %d", i, want.DocumentLineNumber, got.DocumentLineNumber)
-		}
-		if want.Price != got.Price {
-			t.Errorf("For CSV Line %d, wanted              Price: %.2F, but got: %.2F", i, want.Price, got.Price)
-		}
+func TestExtractFloatFieldHandlesNegativeIndex(t *testing.T) {
+	want := float64(10)
+	got, err := extractFloatField([]string{"99.9"}, -1, 10)
+
+	if err != nil {
+		t.Error(err)
+	}
+	if want != got {
+		t.Errorf("extractFloatField should have returned %.2f, but got %.2f instead", want, got)
 	}
 }
