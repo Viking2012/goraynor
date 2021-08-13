a8cdd2426ff4a5ae0fe6ae2fd698c5706481f6b4
Viking2012 <Viking2012@gmail.com>
2021-08-13 09:45:00 -0400

    add sort interface tests

--- src/countr/countr_test.go
+++ src/countr/countr_test.go
@@ -31,6 +31,56 @@ func TestCount(t *testing.T) {
 	}
 }
 
+func TestCounterLen(t *testing.T) {
+	var c = Counter{
+		CounterPair{Value: 1.0, Count: 20.0},
+		CounterPair{Value: 2.0, Count: 10.0},
+	}
+
+	var want int = 2
+	got := c.Len()
+
+	if got != want {
+		t.Errorf("Len should have returned %3d, got %3d instead", want, got)
+	}
+}
+func TestCounterLess(t *testing.T) {
+	var c = Counter{
+		CounterPair{Value: 1.0, Count: 20.0},
+		CounterPair{Value: 2.0, Count: 10.0},
+	}
+
+	var want bool = true
+	var got bool = c.Less(0, 1)
+
+	if got != want {
+		t.Errorf("Less for indices 0 and 1 should have returned %t, got %t instead", want, got)
+	}
+}
+func TestCounterSwap(t *testing.T) {
+	var c = Counter{
+		CounterPair{Value: 2.0, Count: 10.0},
+		CounterPair{Value: 1.0, Count: 20.0},
+	}
+
+	var wantIndices []float64 = []float64{1.0, 2.0}
+	var gotIndices []float64 = make([]float64, c.Len())
+
+	sort.Sort(c)
+	for i := 0; i < c.Len(); i++ {
+		gotIndices[i] = c[i].Value
+	}
+
+	for i := 0; i < c.Len(); i++ {
+		w := wantIndices[i]
+		g := gotIndices[i]
+
+		if g != w {
+			t.Errorf("After Swap, index %d should been %3.1f, got %3.1f instead", i, w, g)
+		}
+	}
+}
+
 func TestNewCounter(t *testing.T) {
 	var want = make(Counter, len(vNums))
 	var toGet = make(map[float64]float64, len(vNums))
